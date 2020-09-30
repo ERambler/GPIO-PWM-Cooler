dc579bf36ae49500f31c4671b89e9a2110b5bc80
ERambler <38523080+ERambler@users.noreply.github.com>
2020-09-30 22:40:08 +0300

    Update gpio_pwm_cooler.cpp

--- gpio_pwm_cooler.cpp
+++ gpio_pwm_cooler.cpp
@@ -15,12 +15,10 @@ float RANGE_COEF= (RANGE_MAX - RANGE_MIN)/(TEMPERATURE_MAX-TEMPERATURE_MIN);
 
 using namespace std;
 
-// Удаление пробелов в строке
 void delSpaces(char *str) 
 {
 for (int i = 0, j = 0; str[i];(str[j++] = str[i] != ' ' ? str[i] : (j--, str[j]), i++, (!str[i] ? (str[j] = 0) : 0)));
 }
-// Функция преобразования строки в целое число
 int StrToInt(char *s)
 {
   int temp = 0, i = 0, sign = 0; // число
@@ -34,14 +32,12 @@ int StrToInt(char *s)
     temp = -temp;
   return(temp);
 }
-//Беру температуру из файла
 static int getTemperature() {
   static fstream myfile;
   int temperature = 0;
   myfile.open("/sys/devices/virtual/thermal/thermal_zone0/temp", ios_base::in);
   myfile >> temperature;
   myfile.close();
-//  cout << temperature/1000;
   return temperature/1000;
 }
 static int getRPM(int T) {
@@ -51,16 +47,15 @@ static int getRPM(int T) {
 	if (rpm > RANGE_MAX) {rpm = RANGE_MAX;}
 return rpm;
 }
-//Забираем конфигурационный файл
 void config_parser ()
 {
     char buff[256];
-    char *parameter;   // буфер промежуточного хранения считываемого из файла текста
-    ifstream fin("/etc/gpio_pwn_cooler.conf"); // открыли файл для чтения
+    char *parameter;   
+    ifstream fin("/etc/gpio_pwn_cooler.conf"); 
  
-    while (fin.getline(buff, 256)) // считали строку из файла
+    while (fin.getline(buff, 256)) 
     {
-    	//cout << buff << " | " << strlen (buff);
+    	
     	delSpaces (buff); //cout << " del spaces";
     	parameter = strtok (buff,"="); //cout << " strtok";
     	//cout << parameter << " |" << sizeof buff << endl;
@@ -71,7 +66,7 @@ void config_parser ()
     	if ((strcmp (parameter,"TEMPERATURE_MAX"))==0) TEMPERATURE_MAX = StrToInt(strtok (NULL,"="));
 
     }
-    fin.close(); // закрываем файл
+    fin.close(); 
     float RANGE_COEF= (RANGE_MAX - RANGE_MIN)/(TEMPERATURE_MAX-TEMPERATURE_MIN);
 }
 
@@ -85,9 +80,7 @@ int main(int argc, char* argv[]) {
         {
         temperature = getTemperature();
         rpm = getRPM(temperature);
-        //cout << "RPM on " << rpm << "% " << "T = " << temperature << "\n";
         softPwmWrite(PIN, rpm);
-        //softPwmStop(PIN);
         usleep(1000 * 5000);
       	}
       }
